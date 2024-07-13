f8b3e8ee316245c58b0e63fb01f029b944a04d3d
bernardoArcari <BernardoArcari@gmail.com>
2024-07-13 02:43:03 -0300

    RabbitMQ added to API

--- PostgresAPP/cmd/api/api.go
+++ PostgresAPP/cmd/api/api.go
@@ -7,6 +7,7 @@ import (
 	"github.com/BernardoDenkvitts/PostgresAPP/internal/infra"
 	"github.com/BernardoDenkvitts/PostgresAPP/internal/route"
 	"github.com/BernardoDenkvitts/PostgresAPP/internal/service"
+	"github.com/BernardoDenkvitts/PostgresAPP/internal/service/rabbitmq"
 	"github.com/BernardoDenkvitts/PostgresAPP/internal/utils"
 )
 
@@ -24,6 +25,16 @@ func (api *ApiServer) Run() error {
 	router := http.NewServeMux()
 
 	storage := setupDatabase()
+
+	rabbitMq, err := infra.NewRabbitMQ()
+	utils.FailOnError(err, "(POSTGRESS APP) Error to instanciate RabbitMQ")
+	defer rabbitMq.Close()
+
+	rabbitMqProducer := rabbitmq.NewRabbitMQProducer(storage, rabbitMq.Channel)
+	go rabbitMqProducer.Produce()
+
+	rabbitMqConsumer := rabbitmq.NewRabbitMQConsumer(storage, rabbitMq.Channel)
+	go rabbitMqConsumer.Consume()
 
 	router.Handle("/postgres/", http.StripPrefix("/postgres", router))
 
